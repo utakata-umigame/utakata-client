ef7e580060a8739aa042d40a9be796498e1f3fcc
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2019-01-11 21:19:51 +0900

    Update UserForm.vue

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,8 +1,8 @@
 <template>
     <form @submit.prevent="confirm">
         <input v-model="user.name" placeholder="ハンドルネーム">
-        <p>削除用パスは暗号化されませんので、知られても大丈夫な文字列をご使用ください。</p>
         <input v-model="user.removePass" placeholder="削除用パス">
+        <p>削除用パスは暗号化されませんので、知られても大丈夫な文字列をご使用ください。</p>
         <input type="submit" value="設定">
     </form>
 </template>
